f6f326973602787e4a3e90984bcf973cc48cc78c
PrayasP29 <prayaspriyadarshi18@gmail.com>
2026-02-18 13:23:53 +0530

    Updated wallet logic

--- Digital Wallet System/wallet.cpp
+++ Digital Wallet System/wallet.cpp
@@ -2,6 +2,7 @@
 #include<string>
 #include<fstream>
 #include<vector>
+#include<sstream>
 #include<iomanip>
 #include<ctime>
 using namespace std;
@@ -12,6 +13,52 @@ string getCurrentDate(){
     dt.pop_back();
     return dt;
 }
+/*----------------------------Transaction-------------------------------------------*/
+class Transaction{
+private:
+    string type;
+    double amount;
+    string date;
+
+public:
+    Transaction(string t, double amt){
+        this->type=t;
+        this->amount=amt;
+        this->date=getCurrentDate();
+    }
+
+    string toFileFormat(string phone){
+        return phone+ '|'+ type + "|"+ to_string(amount)+"|"+date;
+    }
+};
+/*-----------------------------Wallet-----------------------------------------------------*/
+class Wallet{
+private:
+    double balance;
+public:
+    Wallet(double b=0){
+        this->balance=b;
+    }
+    double getBalance(){
+        return balance;
+    }
+
+    void addMoney(double amount){
+        this->balance+=amount;
+    }
+
+    bool dedctMoney(double amount){
+        if(balance>=amount){
+            balance-=amount;
+            return true;
+        }
+        return false;
+    }
+};
+
+
+
+
 
 class User{
 private:
@@ -21,5 +68,11 @@ private:
     string wallet;
 
 public:
-    User(string n, string p, string pi, string w)
+    User(string n, string p, string pi, string w){
+    this->name=n;
+    this->phone=p;
+    this->pin=pi;
+    this->wallet=w;
+
+}
 };
